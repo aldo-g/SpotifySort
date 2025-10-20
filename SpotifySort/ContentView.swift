c890aed162659bcf642a753bc8b109f71b5c4fb8
Alastair Grant <85125580+aldo-g@users.noreply.github.com>
2025-10-20 08:00:29 +0700

    Initial Commit

---
+++ SpotifySort/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotifySort
+//
+//  Created by Ali on 20/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifySort/SpotifySortApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifySortApp.swift
+//  SpotifySort
+//
+//  Created by Ali on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpotifySortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
