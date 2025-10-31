a9e5e6c8b5ed4c84bb9ff6bfa3b546880dd61f4b
Alastair Grant <85125580+aldo-g@users.noreply.github.com>
2025-10-31 11:44:03 +0700

    better injections

--- SpotifySort/App/SpotifySortApp.swift
+++ SpotifySort/App/SpotifySortApp.swift
@@ -2,16 +2,8 @@ import SwiftUI
 
 @main
 struct SpotifySortApp: App {
-    // Core singletons
-    @StateObject private var auth: AuthManager
-    @StateObject private var api: SpotifyAPI
-    @StateObject private var router: Router
-    @StateObject private var previews: PreviewResolver
-    @StateObject private var metadata: TrackMetadataService
-
-    // New unified environment
     @StateObject private var env: AppEnvironment
-
+    
     init() {
         let auth = AuthManager()
         let api = SpotifyAPI()
@@ -19,12 +11,7 @@ struct SpotifySortApp: App {
         let previews = PreviewResolver(api: api)
         let metadata = TrackMetadataService()
         let env = AppEnvironment(auth: auth, api: api, router: router, previews: previews, metadata: metadata)
-
-        _auth = StateObject(wrappedValue: auth)
-        _api = StateObject(wrappedValue: api)
-        _router = StateObject(wrappedValue: router)
-        _previews = StateObject(wrappedValue: previews)
-        _metadata = StateObject(wrappedValue: metadata)
+        
         _env = StateObject(wrappedValue: env)
     }
 
@@ -32,8 +19,8 @@ struct SpotifySortApp: App {
         WindowGroup {
             RootView()
                 .environmentObject(env)
-                .task { await auth.resumeSession() }
-                .onOpenURL { url in auth.handleRedirect(url: url) }
+                .task { await env.auth.resumeSession() }
+                .onOpenURL { url in env.auth.handleRedirect(url: url) }
         }
     }
 }
